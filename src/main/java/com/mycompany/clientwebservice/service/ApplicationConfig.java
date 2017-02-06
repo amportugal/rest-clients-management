d31f3220d0eb66d59a564eb8f7896e8131927ee4
Nuno <nunommarquespt@gmail.com>
2017-02-06 01:54:15 +0000

    Swagger documentation added. Needing revision.

--- src/main/java/com/mycompany/clientwebservice/service/ApplicationConfig.java
+++ src/main/java/com/mycompany/clientwebservice/service/ApplicationConfig.java
@@ -5,6 +5,9 @@
  */
 package com.mycompany.clientwebservice.service;
 
+import io.swagger.jaxrs.config.BeanConfig;
+import io.swagger.jaxrs.listing.ApiListingResource;
+import io.swagger.jaxrs.listing.SwaggerSerializers;
 import java.util.Set;
 import javax.ws.rs.core.Application;
 
@@ -15,10 +18,27 @@ import javax.ws.rs.core.Application;
 @javax.ws.rs.ApplicationPath("webresources")
 public class ApplicationConfig extends Application {
 
+    public ApplicationConfig() {
+        BeanConfig conf = new BeanConfig();
+        conf.setTitle("Clients API");
+        conf.setDescription("Client management web service");
+        conf.setVersion("1.0.0");
+        conf.setHost("localhost:8080");
+        conf.setBasePath("/ClientWebService/webresources");
+        conf.setSchemes(new String[] { "http" });
+        conf.setResourcePackage("com.mycompany.clientwebservice.service");
+        conf.setScan(true);
+    }
+
     @Override
     public Set<Class<?>> getClasses() {
         Set<Class<?>> resources = new java.util.HashSet<>();
         addRestResourceClasses(resources);
+        
+        
+        //Swagger classes
+        resources.add(ApiListingResource.class);
+        resources.add(SwaggerSerializers.class);
         return resources;
     }
 
--- src/main/java/com/mycompany/clientwebservice/service/ClientsREST.java
+++ src/main/java/com/mycompany/clientwebservice/service/ClientsREST.java
@@ -6,6 +6,12 @@
 package com.mycompany.clientwebservice.service;
 
 import com.mycompany.clientwebservice.Client;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import io.swagger.annotations.ResponseHeader;
 import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
@@ -28,7 +34,8 @@ import javax.ws.rs.core.MediaType;
  * @author nuno
  */
 @Stateless
-@Path("clients")
+@Api
+@Path("/clients")
 public class ClientsREST{
 
     @PersistenceContext(unitName = "com.mycompany_ClientWebService_war_1.0-SNAPSHOTPU")
@@ -37,72 +44,167 @@ public class ClientsREST{
     public ClientsREST() {
     }
 
+    @ApiOperation(
+        value = "Creates a new client on the database", 
+        consumes = MediaType.APPLICATION_JSON + "," + MediaType.APPLICATION_XML)
+    @ApiResponses(value = {
+        @ApiResponse(
+          code=204,
+          message="New client created"), 
+        @ApiResponse(
+          code=500,
+          message="New client not created, due to a wrong formatted payload or "
+                  + "invalid values"), })
     @POST
     @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
-    public void createClient(Client entity) {
+    public void createClient(
+            @ApiParam(
+                    value="Client",
+                    name="entity",
+                    required=true)
+            Client entity) {
         getEntityManager().persist(entity);
     }
 
+    @ApiOperation(
+        value = "Edits an existing client on the database", 
+        consumes = MediaType.APPLICATION_JSON + "," + MediaType.APPLICATION_XML)
+    @ApiResponses(value = {
+        @ApiResponse(
+          code=204,
+          message="Client information updated"), 
+        @ApiResponse(
+          code=500,
+          message="Client information not updated, due to a wrong formatted payload, "
+                  + "invalid values or an id to a non-existing client"), })
     @PUT
     @Path("{nif}")
     @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
-    public void editClient(@PathParam("nif") Integer id, Client entity) {
+    public void editClient(
+            @ApiParam(
+                    value="Integer",
+                    name="nif",
+                    required=true)
+            @PathParam("nif") Integer id, 
+            @ApiParam(
+                    value="Client",
+                    name="entity",
+                    required=true)
+            Client entity) {
+        Client entity2 = getEntityManager().find(Client.class, id);
+        getEntityManager().merge(entity2);
         getEntityManager().merge(entity);
     }
 
+    @ApiOperation(
+        value = "Removes an existing client from the database", 
+        consumes = MediaType.APPLICATION_JSON + "," + MediaType.APPLICATION_XML)
+    @ApiResponses(value = {
+        @ApiResponse(
+          code=204,
+          message="Client removed from the database"), 
+        @ApiResponse(
+          code=500,
+          message="Client not deleted, due to an id to a non-existing client"), })
     @DELETE
     @Path("{nif}")
-    public void removeClient(@PathParam("nif") Integer id) {
+    public void removeClient(
+            @ApiParam(
+                    value="Integer",
+                    name="nif",
+                    required=true)
+            @PathParam("nif") Integer id ) {
         getEntityManager().remove(getEntityManager().merge(getEntityManager().find(Client.class, id)));
     }
     
-    @GET
-    @Path("find/{id}")
-    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
-    public Client find(@PathParam("id") Integer id) {
-        return getEntityManager().find(Client.class, id);
-    }
+//    @GET
+//    @Path("find/{id}")
+//    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+//    public Client find(@PathParam("id") Integer id) {
+//        return getEntityManager().find(Client.class, id);
+//    }
 
+    @ApiOperation(
+        value = "Gets all the clients currently on the database", 
+        consumes = MediaType.APPLICATION_JSON + "," + MediaType.APPLICATION_XML)
+    @ApiResponses(value = {
+        @ApiResponse(
+          code=200,
+          message="Returns a list of clients",
+          response=Client.class,
+          responseContainer="List")})
     @GET
-    @Path("{nif}")
     @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
-    public List<Client> getClients(@PathParam("nif") Integer id) {
+    public List<Client> getClients() {
         Query q = getEntityManager().createNamedQuery("listAllClients");
         List<Client> clients = q.getResultList();
         
         return clients;
     }
 
+    @ApiOperation(
+        value = "Gets the client with a certain NIF", 
+        consumes = MediaType.APPLICATION_JSON + "," + MediaType.APPLICATION_XML)
+    @ApiResponses(value = {
+        @ApiResponse(
+          code=200,
+          message="Returns the client",
+          response=Client.class),
+        @ApiResponse(
+          code=500,
+          message="Client has not been found with the provided NIF")})
     @GET
     @Path("getClientFromNIF/{nif}")
     @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
-    public Client getClientFromNIF(@PathParam("nif") Integer id) {
+    public Client getClientFromNIF(
+            @ApiParam(
+                    value="Integer",
+                    name="nif",
+                    required=true)
+            @PathParam("nif") Integer id) {
+        //TODO: ArrayIndexOutOfBounds exception
         Query q = getEntityManager().createNamedQuery("getClientFromNIF").setParameter(1, id);
         Client client = (Client) q.getResultList().get(0);
         
         return client;
     }
 
+    @ApiOperation(
+        value = "Gets all the clients that have a certain name", 
+        consumes = MediaType.APPLICATION_JSON + "," + MediaType.APPLICATION_XML)
+    @ApiResponses(value = {
+        @ApiResponse(
+          code=200,
+          message="Returns all the clients that match the criteria",
+          response=Client.class),
+        @ApiResponse(
+          code=500,
+          message="No client has been found with the provided name")})
     @GET
     @Path("getClientFromName/{name}")
     @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
-    public List<Client> getClientsFromName(@PathParam("name") String name) {
+    public List<Client> getClientsFromName(
+            @ApiParam(
+                    value="String",
+                    name="name",
+                    required=true)
+            @PathParam("name") String name) {
         Query q = getEntityManager().createNamedQuery("getClientsFromName").setParameter(1, name);
         List<Client> clients = q.getResultList();
         
         return clients;
     }
 
-    @GET
-    @Path("count")
-    @Produces(MediaType.TEXT_PLAIN)
-    public String countREST() {
-        CriteriaQuery cq = getEntityManager().getCriteriaBuilder().createQuery();
-        Root<Client> rt = cq.from(Client.class);
-        cq.select(getEntityManager().getCriteriaBuilder().count(rt));
-        javax.persistence.Query q = getEntityManager().createQuery(cq);
-        return q.getSingleResult().toString();
-    }
+//    @GET
+//    @Path("count")
+//    @Produces(MediaType.TEXT_PLAIN)
+//    public String countREST() {
+//        CriteriaQuery cq = getEntityManager().getCriteriaBuilder().createQuery();
+//        Root<Client> rt = cq.from(Client.class);
+//        cq.select(getEntityManager().getCriteriaBuilder().count(rt));
+//        javax.persistence.Query q = getEntityManager().createQuery(cq);
+//        return q.getSingleResult().toString();
+//    }
 
     protected EntityManager getEntityManager() {
         return em;
